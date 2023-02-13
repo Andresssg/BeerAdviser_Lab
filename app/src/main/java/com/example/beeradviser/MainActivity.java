b5681d4017c3ae406a118b08d6325a619261fba1
Andresssg <andres-guevara1@upc.edu.co>
2023-02-13 00:25:53 -0500

    Finish beer adviser app

--- app/src/main/java/com/example/beeradviser/MainActivity.java
+++ app/src/main/java/com/example/beeradviser/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.beeradviser;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.hfad.beeradviser.BeerExpert;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickFindBeer(View view) {
+        //Get txt view
+        TextView brands = findViewById(R.id.beerTxtView);
+        //Get Spinner
+        Spinner listBeerType = findViewById(R.id.beerSpinner);
+        //Convert spinner text to string
+        String beerType = String.valueOf(listBeerType.getSelectedItem());
+        //Create new Beer Expert class
+        BeerExpert beerExpert = new BeerExpert();
+        //Find recommended beers by type of beer
+        List<String> recommendedBeers = beerExpert.getBrands(beerType);
+        //Set list in text view
+        brands.setText("");
+        for (String beer : recommendedBeers) {
+            System.out.println(beer);
+            brands.append("- " + beer + "\n");
+        }
+
     }
 }
